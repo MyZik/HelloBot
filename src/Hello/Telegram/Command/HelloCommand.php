2e2e393a9c05530e6be99b78e3a62ef5cedafe09
eldarQa <eldar@polargold.de>
2019-10-30 08:56:43 +0100

    First commit

---
+++ src/Hello/Telegram/Command/HelloCommand.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Hello\Telegram\Command;
+
+use BoShurik\TelegramBotBundle\Telegram\Command\AbstractCommand;
+use BoShurik\TelegramBotBundle\Telegram\Command\PublicCommandInterface;
+use TelegramBot\Api\BotApi;
+use TelegramBot\Api\Types\Update;
+
+class HelloCommand extends AbstractCommand implements PublicCommandInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getName()
+    {
+        return '/hello';
+    }
+    /**
+     * @inheritDoc
+     */
+    public function getDescription()
+    {
+        return 'Example command';
+    }
+
+    /**
+     * @param BotApi $api
+     * @param Update $update
+     * @return mixed|void
+     * @throws \TelegramBot\Api\Exception
+     * @throws \TelegramBot\Api\InvalidArgumentExceptiony
+     */
+    public function execute(BotApi $api, Update $update)
+    {
+        preg_match(self::REGEXP, $update->getMessage()->getText(), $matches);
+        $who = !empty($matches[3]) ? $matches[3] : "World";
+        $text = sprintf('Hello *%s*', $who);
+        $api->sendMessage($update->getMessage()->getChat()->getId(), 'Teststststs', 'markdown');
+    }
+}
